fbd35ccb98590d0f916d31ec9c4a49876b4a266f
Johannes Elferich <jojotux123@hotmail.com>
2021-11-05 10:15:09 -0400

    First functional unit

--- src/napari_cryofibsem_monitor/_dock_widget.py
+++ src/napari_cryofibsem_monitor/_dock_widget.py
@@ -7,8 +7,13 @@ see: https://napari.org/docs/dev/plugins/hook_specifications.html
 Replace code below according to your needs.
 """
 from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton, QFileDialog
 from magicgui import magic_factory
+
+import glob
+import os
+import tifffile
+import numpy as np
 
 
 class ExampleQWidget(QWidget):
@@ -19,15 +24,37 @@ class ExampleQWidget(QWidget):
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        btn_autotem_directory = QPushButton("Select AutoTEM directort")
+        btn_autotem_directory.clicked.connect(self._on_click)
 
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        self.layout().addWidget(btn_autotem_directory)
 
     def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        directory = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        site_directories = glob.glob(os.path.join(directory,"Sites/*/"))
+        im_data = []
+        for site_directory in site_directories:
+            
+            name = os.path.split(site_directory)[-1]
+            dci_images = sorted(glob.glob(os.path.join(site_directory,"DCImages/*/*.tif")))
+            if len(dci_images) < 1:
+                continue
+            im_data.append([])
+            for image_fn in dci_images:
+                im = tifffile.imread(image_fn)
+                if im.dtype == 'uint8':
+                    im_data[-1].append(im)
+        (y , x) = im_data[-1][-1].shape
+        num_sites = len(im_data)
+        num_img = max([len(a) for a in im_data])
+
+        im = np.zeros((num_sites,num_img,y,x))
+        for i,site in enumerate(im_data):
+            for j,mage in enumerate(site):
+                im[i,j,:,:] = mage
+        
+        self.viewer.add_image(im,name="lamellas")
 
 
 @magic_factory
